39e05abe86bcc2f4501b16dc273f89a717489dee
AkshatMishra01 <mishraakshat924@gmail.com>
2021-02-25 14:39:39 +0530

    Third commit

---
+++ BRM_lab3/src/main/java/AppTest.java
@@ -0,0 +1,8 @@
+public class AppTest{
+	public int add(int a,int b) {
+		return a+b; 
+	}
+	public int minus(int x,int y) {
+		return x - y;
+	}
+}
---
+++ BRM_lab3/src/test/java/AppTest1.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest1 {
+
+	@Test
+	public void test() {
+		AppTest obj = new AppTest();
+		int result = obj.add(4, 5);
+		System.out.println(result);
+	}
+
+}
